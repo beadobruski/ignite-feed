17882fb9e5cb70027adf9e38342aa9a87a89c742
Beatriz Dobruski <bbeadobruski@gmail.com>
2022-06-30 18:33:12 -0300

    fix: Use State error

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -5,7 +5,7 @@ import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
-  const [likeCount, setLikeCount] = useStatete(0);
+  const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
     onDeleteComment(content);
